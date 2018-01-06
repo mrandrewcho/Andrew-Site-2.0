cf34ba2f602f0fe0e2f7154c9c87332b8c8e7259
mrandrewcho <andycho7@gmail.com>
2018-01-05 23:39:06 -0500

    push push baby

--- js/resume.js
+++ js/resume.js
@@ -25,4 +25,10 @@
     target: '#sideNav'
   });
 
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#topNav'
+  });
+
+
 })(jQuery); // End of use strict
